41f91414b029172d9f05bb119f750f24fc46e237
Elio <elio.antonio.654@gmail.com>
2025-05-13 17:33:24 -0600

    feat: Add a form to add new students in the DialogAddStudent component

--- src/components/user/DialogAddStudent.tsx
+++ src/components/user/DialogAddStudent.tsx
@@ -1,5 +1,18 @@
 // DialogAddStudent.tsx
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from '@mui/material';
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  TextField,
+  Box,
+  InputLabel,
+  Select,
+  SelectChangeEvent,
+  MenuItem,
+} from "@mui/material";
+import React from "react";
 
 interface DialogAddStudentProps {
   open: boolean;
@@ -7,15 +20,43 @@ interface DialogAddStudentProps {
 }
 
 export function DialogAddStudent({ open, onClose }: DialogAddStudentProps) {
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
   return (
     <Dialog open={open} onClose={onClose}>
       <DialogTitle>Agregar nuevo alumno</DialogTitle>
       <DialogContent>
-        {/* Contenido del formulario */}
+        <Box
+          component="form"
+          sx={{ "& > :not(style)": { m: 1, width: "25ch" } }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField id="standard-basic" label="Matrícula" variant="standard" />
+          <TextField id="standard-basic" label="Nombre" variant="standard" />
+          <TextField id="standard-basic" label="Correo" variant="standard" />
+          <InputLabel id="demo-simple-select-label">Club</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={age}
+            label="Age"
+            onChange={handleChange}
+          >
+            <MenuItem value={'club_ajedrez'}>Ajedrez</MenuItem>
+            <MenuItem value={'club_danza'}>Danza</MenuItem>
+            <MenuItem value={'club_taekwondo'}>Taekwondo</MenuItem>
+          </Select>
+        </Box>{" "}
       </DialogContent>
       <DialogActions>
         <Button onClick={onClose}>Cancelar</Button>
-        <Button onClick={onClose} variant="contained">Guardar</Button>
+        <Button onClick={onClose} variant="contained">
+          Guardar
+        </Button>
       </DialogActions>
     </Dialog>
   );
