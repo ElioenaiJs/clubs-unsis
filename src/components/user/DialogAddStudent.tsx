62a20e080478f807ac5bcd5e61c67255c57b4619
Elio <elio.antonio.654@gmail.com>
2025-05-13 17:47:12 -0600

    feat: Reset age state on close in DialogAddStudent component

--- src/components/user/DialogAddStudent.tsx
+++ src/components/user/DialogAddStudent.tsx
@@ -1,86 +1,92 @@
 // DialogAddStudent.tsx
 import {
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  DialogActions,
-  Button,
-  TextField,
-  Box,
-  InputLabel,
-  Select,
-  SelectChangeEvent,
-  MenuItem,
-  FormControl,
-} from "@mui/material";
-import React from "react";
-
-interface DialogAddStudentProps {
-  open: boolean;
-  onClose: () => void;
-}
-
-export function DialogAddStudent({ open, onClose }: DialogAddStudentProps) {
-  const [age, setAge] = React.useState("");
-
-  const handleChange = (event: SelectChangeEvent) => {
-    setAge(event.target.value as string);
-  };
-  return (
-    <Dialog open={open} onClose={onClose} slotProps={{ paper: { sx: { width: "600px", maxWidth: "none", }}}}>
-      <DialogTitle>Agregar nuevo alumno</DialogTitle>
-      <DialogContent>
-        <Box
-          component="form"
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            gap: 2,
-            width: "100%",
-          }}
-          noValidate
-          autoComplete="off"
-        >
-          <TextField
-            id="matricula"
-            label="Matrícula"
-            variant="standard"
-            fullWidth
-          />
-          <TextField
-            id="nombre"
-            label="Nombre"
-            variant="standard"
-            fullWidth
-          />
-          <TextField
-            id="correo"
-            label="Correo"
-            variant="standard"
-            fullWidth
-          />
-          <FormControl fullWidth variant="standard">
-            <InputLabel id="club-select-label">Club</InputLabel>
-            <Select
-              labelId="club-select-label"
-              id="club-select"
-              value={age}
-              label="Club"
-              onChange={handleChange}
-            >
-              <MenuItem value={"club_ajedrez"}>Ajedrez</MenuItem>
-              <MenuItem value={"club_danza"}>Danza</MenuItem>
-              <MenuItem value={"club_taekwondo"}>Taekwondo</MenuItem>
-            </Select>
-          </FormControl>
-        </Box>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={onClose}>Cancelar</Button>
-        <Button onClick={onClose} variant="contained">
-          Guardar
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
-}
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogActions,
+    Button,
+    TextField,
+    Box,
+    InputLabel,
+    Select,
+    SelectChangeEvent,
+    MenuItem,
+    FormControl,
+  } from "@mui/material";
+  import React from "react";
+  
+  interface DialogAddStudentProps {
+    open: boolean;
+    onClose: () => void;
+  }
+  
+  export function DialogAddStudent({ open, onClose }: DialogAddStudentProps) {
+    const [age, setAge] = React.useState("");
+  
+    const handleChange = (event: SelectChangeEvent) => {
+      setAge(event.target.value as string);
+    };
+  
+    const handleClose = () => {
+      setAge(""); 
+      onClose();
+    };
+  
+    return (
+      <Dialog open={open} onClose={handleClose} slotProps={{ paper: { sx: { width: "600px", maxWidth: "none" } } }}>
+        <DialogTitle>Agregar nuevo alumno</DialogTitle>
+        <DialogContent>
+          <Box
+            component="form"
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: 2,
+              width: "100%",
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <TextField
+              id="matricula"
+              label="Matrícula"
+              variant="standard"
+              fullWidth
+            />
+            <TextField
+              id="nombre"
+              label="Nombre"
+              variant="standard"
+              fullWidth
+            />
+            <TextField
+              id="correo"
+              label="Correo"
+              variant="standard"
+              fullWidth
+            />
+            <FormControl fullWidth variant="standard">
+              <InputLabel id="club-select-label">Club</InputLabel>
+              <Select
+                labelId="club-select-label"
+                id="club-select"
+                value={age}
+                label="Club"
+                onChange={handleChange}
+              >
+                <MenuItem value={"club_ajedrez"}>Ajedrez</MenuItem>
+                <MenuItem value={"club_danza"}>Danza</MenuItem>
+                <MenuItem value={"club_taekwondo"}>Taekwondo</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancelar</Button>
+          <Button onClick={handleClose} variant="contained">
+            Guardar
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
